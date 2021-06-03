d95886d57fcdfbb54b4ed2d1e5cd9ec46ae7e150
StasyaRom <81742852+StasyaRom@users.noreply.github.com>
2021-06-03 16:42:38 +0300

    Update index.js

--- homework3/index.js
+++ homework3/index.js
@@ -84,6 +84,19 @@ function arrayUnique(arr) {
 arrayUnique([])
 
 
+//task7
+function lastElem(arr, count){
+    if (count == null){
+        console.log(arr.pop());
+    }else{
+        console.log(arr.slice(-count));
+    }
+}
+console.log(lastElem([3, 4, 10, -5]));
+console.log(lastElem([3, 4, 10, -5], 2));
+
+
+
 // task8
 function firstLetterUpCase(str) {
 return str
